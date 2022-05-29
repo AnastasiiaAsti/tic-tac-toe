1f1e730b4eddbfcda1f9f53525b78e41e24f4903
Asti <astistateofmind@gmail.com>
2022-05-29 13:56:16 -0700

    stump up functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 /*----- constants -----*/
+//players 
+const PLAYER_X = 'x'
+const PLAYER_O = 'o'
+
+const RESET_BTN = document.getElementById('reset')
+
 const CURRENT_TURN = ''
 const WINNING_MESSAGE = ''
-const DRAW_MESSAGE =  ''
+const DRAW_MESSAGE = ''
+
+//there are 8 winning combinations:
+const WINNING_COMBO = [
+    ['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'],
+    ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'],
+    ['0', '4', '8'], ['2', '4', '6']    
+]
+
+let currentPlayer = ''
 
 
 
@@ -10,14 +25,39 @@ const DRAW_MESSAGE =  ''
 
 
 /*----- cached element references -----*/
-
+const board = document.getElementById('board')
 
 
 /*----- event listeners -----*/
-
+//adding eventListener to each cell
+board.addEventListener('click', handleClick)
+//add an eventListener to a RESET btn
+document.getElementById('reset').addEventListener('click', handleReset)
 
 
 /*----- functions -----*/
+//create a function for event listener for the board
+function handleClick(evt) {
+    //console.log(evt.target)
+
+}
+//we need a function that will handle the cell that was pressed
+function handlePressedCell() { }
+//a function to check results
+function handleCheckResults() { }
+//a function to change the player
+function handlePlayerChange() { }
+//a function to reset the game
+function handleReset() { }
+
+
+function render {
+
+}
+
+function init() {
+    
+}
 
 
 //1. track the clicks on each cell
@@ -29,6 +69,6 @@ const DRAW_MESSAGE =  ''
 //7. clear the board and repeat
 //8. reflect updates in User Interface 
 
-//there is 8 winning combinations
+
 
 console.log('js is loaded')
