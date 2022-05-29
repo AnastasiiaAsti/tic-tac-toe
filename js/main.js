e697dec65f1b53ea54e855dc8a4f4cfe1e1e4865
Asti <astistateofmind@gmail.com>
2022-05-29 13:07:58 -0700

    change in style

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*----- constants -----*/
+const CURRENT_TURN = ''
+const WINNING_MESSAGE = ''
+const DRAW_MESSAGE =  ''
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+
+//1. track the clicks on each cell
+//2. cell can only be clicked once
+//3. need to update the state of the game
+//4. check and display the result of the game (who win, or if its even)
+//5. need to stop the game after someone won
+//6. restart the game after pressing a button
+//7. clear the board and repeat
+//8. reflect updates in User Interface 
+
+//there is 8 winning combinations
+
+console.log('js is loaded')
