878515fa8c2a2b8df2d5fd66b9dc713178e54cba
Asti <astistateofmind@gmail.com>
2022-06-02 09:37:26 -0700

    swaping main and test js pages

--- js/main.js
+++ js/main.js
@@ -1,107 +1,100 @@
 /*----- constants -----*/
-//players 
-const PLAYER_X = 'x'
-const PLAYER_O = 'o'
 const CELL_ELEMENTS = document.querySelectorAll('.cell')
 const RESET_BTN = document.getElementById('reset')
 
-// const CURRENT_TURN = `${currentPlayer}'s turn`
-// const WINNING_MESSAGE = `Player ${currentPlayer} is the WINNER!`
-// const DRAW_MESSAGE = 'Shake hands, we are all winners here'
+//players 
+const PLAYER_X = 'x';
+const PLAYER_O = 'o';
 
-//there are 8 winning combinations:
-const WINNING_COMBO = [
-    ['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'],
-    ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'],
-    ['0', '4', '8'], ['2', '4', '6']    
-]
 
-let currentPlayer = PLAYER_X
-let playerOTurn
+let currentPlayer = PLAYER_X;
 
+const BOARD = (new Array(9).fill(null))
 
 
 /*----- app's state (variables) -----*/
-//a varibale for a current game state that I will update in every step of
-//the game. For now an emply array.
-let gameStates = ['', '', '', '', '', '', '', '', ''];
+//let gameStates = ['', '', '', '', '', '', '', '', ''];
 
 
 /*----- cached element references -----*/
-const board = document.getElementById('board')
+
 
 
 /*----- event listeners -----*/
-//adding eventListener to each cell
-//we iterating over each cell element and adding an event listener
-//{once: true} - is helping to make sure the cell will not be clicked twice
-CELL_ELEMENTS.forEach(cell => {
-    cell.addEventListener('click', handleClick, {once: true})
-})
-//add an eventListener to a RESET btn
-document.getElementById('reset').addEventListener('click', handleReset)
+//add event listener to listen to a clicked cell
+CELL_ELEMENTS.forEach((cell) => cell.addEventListener('click', CLICKED_BOX))
 
 
 /*----- functions -----*/
-//create a function for event listener for the board
-function handleClick(evt) {
-    //console.log(evt.target)
-    //const for the cell that was already clicked
-    const clickedCell = evt.target;
-    //will use 'data-value' on the cells to identify which cell is which
-    //parseInt will transform 'string' into a number
-    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-value')
-    );
-    //checking if the cell was clicked or if the game is over
-    
-        //if not those 2 options - game continues
-        handlePressedCell(clickedCell, clickedCellIndex);
-    handleCheckResults();
-}
+function CLICKED_BOX(evt) {
+    const id = evt.target.id;
 
-
-//we need a function that will handle the cell that was pressed
-function handlePressedCell(clickedCell, clickedCellIndex) { 
-    //updating gameState to display that cell was clicked
-    gameStates[clickedCellIndex] = currentPlayer;
-    //updating UI to show the move
-    clickedCell.innerHTML = currentPlayer;
-    //console.log(handlePressedCell)
-    handlePlayerChange()
-}
-//a function to check results
-function handleCheckResults() { 
-
-}
-//a function to change the player
-function handlePlayerChange() { 
-    if (currentPlayer === PLAYER_X) {
-        currentPlayer = PLAYER_O;
-    } else {
-        currentPlayer = PLAYER_X
+    if (!BOARD[id]) {
+        BOARD[id] = currentPlayer;
+        evt.target.innerText = currentPlayer;
+        if (checkResult()) endGame();
+        if (!BOARD.some((evt) => evt === null)) endGame('draw');
+        //switching players
+        if (currentPlayer === PLAYER_X) {
+            currentPlayer = PLAYER_O;
+        } else {
+            currentPlayer = PLAYER_X
+        }
     }
+    console.log(BOARD)
+};
+
+
+//if result is draw => return 'DRAW' if otherwose -> currentPlayer won
+function endGame(result) {
+    document.getElementById('game-status').innerText = result == 'draw' ? 'Draw!' : currentPlayer + ' won';
+    CELL_ELEMENTS.forEach((cell) => cell.removeEventListener('click', CLICKED_BOX))
+    //console.log('End of the Game')
 }
-//a function to reset the game
+
 function handleReset() {
-}
+    currentPlayer = 0;
+    board.fill(null);
+    CELL_ELEMENTS.forEach((cell) => {
+        cell.innerText = '';
+    });
+    document.getElementById('game-status').innerText = 'Lets play';
+    CELL_ELEMENTS.forEach((cell) => cell.addEventListener('click', CLICKED_BOX))
+};
 
+//there are 8 winning combinations:
+// const WINNING_COMBO = [
+//     ['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'],
+//     ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'],
+//     ['0', '4', '8'], ['2', '4', '6']    
+// ]
 
-function render() {}
-
-function init() {
+function checkResult() {
+    if (currentPlayer == BOARD[0] && BOARD[0] == BOARD[1] && BOARD[0] == BOARD[2]) 
+        return true;
     
-}
+    if (currentPlayer == BOARD[3] && BOARD[3] == BOARD[4] && BOARD[3] == BOARD[5])
+        return true;
+    
+    if (currentPlayer == BOARD[6] && BOARD[6] == BOARD[7] && BOARD[6] == BOARD[8])
+        return true;
+    
+    if (currentPlayer == BOARD[0] && BOARD[0] == BOARD[3] && BOARD[0] == BOARD[6])
+        return true;
+    
+    if (currentPlayer == BOARD[1] && BOARD[1] == BOARD[4] && BOARD[1] == BOARD[7])
+        return true;
+    
+    if (currentPlayer == BOARD[2] && BOARD[2] == BOARD[5] && BOARD[2] == BOARD[8])
+        return true;
+    
+    if (currentPlayer == BOARD[0] && BOARD[0] == BOARD[4] && BOARD[0] == BOARD[8])
+        return true;
+    
+    if (currentPlayer == BOARD[2] && BOARD[2] == BOARD[4] && BOARD[2] == BOARD[6])
+        return true;
+    return false;
+};
 
-
-//1. track the clicks on each cell
-//2. cell can only be clicked once
-//3. need to update the state of the game
-//4. check and display the result of the game (who win, or if its even)
-//5. need to stop the game after someone won
-//6. restart the game after pressing a button
-//7. clear the board and repeat
-//8. reflect updates in User Interface 
-
-
-
-console.log('js is loaded')
+CELL_ELEMENTS.forEach((cell) => cell.addEventListener('click', CLICKED_BOX));
+RESET_BTN.addEventListener('click', handleReset);
--- js/test.js
+++ js/test.js
@@ -1,34 +1,107 @@
 /*----- constants -----*/
-const CELL_ELEMENTS = document.querySelectorAll('.cell')
-
 //players 
 const PLAYER_X = 'x'
 const PLAYER_O = 'o'
+const CELL_ELEMENTS = document.querySelectorAll('.cell')
+const RESET_BTN = document.getElementById('reset')
 
-let currentPlayer = PLAYER_X;
+// const CURRENT_TURN = `${currentPlayer}'s turn`
+// const WINNING_MESSAGE = `Player ${currentPlayer} is the WINNER!`
+// const DRAW_MESSAGE = 'Shake hands, we are all winners here'
 
-const CLICKED_BOX = (evt) => {
-    const IDX = evt.target.id;
+//there are 8 winning combinations:
+const WINNING_COMBO = [
+    ['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'],
+    ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'],
+    ['0', '4', '8'], ['2', '4', '6']    
+]
 
-    evt.target.innerText = currentPlayer;
+let currentPlayer = PLAYER_X
+let playerOTurn
+
+
+
+/*----- app's state (variables) -----*/
+//a varibale for a current game state that I will update in every step of
+//the game. For now an emply array.
+let gameStates = ['', '', '', '', '', '', '', '', ''];
+
+
+/*----- cached element references -----*/
+const board = document.getElementById('board')
+
+
+/*----- event listeners -----*/
+//adding eventListener to each cell
+//we iterating over each cell element and adding an event listener
+//{once: true} - is helping to make sure the cell will not be clicked twice
+CELL_ELEMENTS.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true})
+})
+//add an eventListener to a RESET btn
+document.getElementById('reset').addEventListener('click', handleReset)
+
+
+/*----- functions -----*/
+//create a function for event listener for the board
+function handleClick(evt) {
+    //console.log(evt.target)
+    //const for the cell that was already clicked
+    const clickedCell = evt.target;
+    //will use 'data-value' on the cells to identify which cell is which
+    //parseInt will transform 'string' into a number
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-value')
+    );
+    //checking if the cell was clicked or if the game is over
+    
+        //if not those 2 options - game continues
+        handlePressedCell(clickedCell, clickedCellIndex);
+    handleCheckResults();
+}
+
+
+//we need a function that will handle the cell that was pressed
+function handlePressedCell(clickedCell, clickedCellIndex) { 
+    //updating gameState to display that cell was clicked
+    gameStates[clickedCellIndex] = currentPlayer;
+    //updating UI to show the move
+    clickedCell.innerHTML = currentPlayer;
+    //console.log(handlePressedCell)
+    handlePlayerChange()
+}
+//a function to check results
+function handleCheckResults() { 
+
+}
+//a function to change the player
+function handlePlayerChange() { 
     if (currentPlayer === PLAYER_X) {
         currentPlayer = PLAYER_O;
     } else {
         currentPlayer = PLAYER_X
     }
-};
-
-/*----- app's state (variables) -----*/
-let gameStates = ['', '', '', '', '', '', '', '', ''];
-
-
-/*----- cached element references -----*/
+}
+//a function to reset the game
+function handleReset() {
+}
 
 
+function render() {}
 
-/*----- event listeners -----*/
-//add event listener to listen to a clicked cell
-CELL_ELEMENTS.forEach((cell) => cell.addEventListener('click', CLICKED_BOX))
+function init() {
+    
+}
 
 
-/*----- functions -----*/
+//1. track the clicks on each cell
+//2. cell can only be clicked once
+//3. need to update the state of the game
+//4. check and display the result of the game (who win, or if its even)
+//5. need to stop the game after someone won
+//6. restart the game after pressing a button
+//7. clear the board and repeat
+//8. reflect updates in User Interface 
+
+
+
+console.log('js is loaded')
